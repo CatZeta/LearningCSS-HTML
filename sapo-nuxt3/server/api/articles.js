3434878797f05dfbca39d1339c0595ed4185494d
Catarina Roseta <catarinaroseta.dev@gmail.com>
2024-07-03 13:58:21 +0100

    Establish connection to db through /api/articles - quering db on server-side

--- sapo-nuxt3/server/api/articles.js
+++ sapo-nuxt3/server/api/articles.js
@@ -1,7 +1,33 @@
 // server/api/articles.js
+import { createClient } from '@supabase/supabase-js'
 
-import {useGetArticles} from '~/composables/useGetArticles'
 
 export default defineEventHandler(async (event) => {
-    return {message: `Hello World`}
+    //Initialize Supabase on the server-side
+    const config = useRuntimeConfig()
+    const supabaseUrl = config.public.supabaseUrl
+    const supabaseKey = config.public.supabaseKey
+    const supabase = createClient(supabaseUrl, supabaseKey)
+
+    const { data: articles, error } = await supabase
+        .from('articles')
+        .select(`
+      id,
+      publishedAt,
+      source_id,
+      source_name,
+      author,
+      title,
+      description,
+      url,
+      urlToImage,
+      content
+    `)
+
+    if (error) {
+        console.error('Error fetching articles:', error)
+        return []
+    }
+
+    return articles
 })
